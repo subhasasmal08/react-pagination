f25172ad909fec0edadd8bb44f9f9321bd865c00
subhasasmal08 <subhasasmal08@gmail.com>
2023-04-17 23:29:10 +0530

    pagination changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
-import './App.css';
-import axios from 'axios';
-const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
+import logo from "./logo.svg";
+import "./App.css";
+import PaginationComponent from "./components/PaginationComponent";
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-  const paginate = pageNumber => setCurrentPage(pageNumber);
-
+function App() {
   return (
-    <div>
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
+    <div className="App">
+      <div className="App-header">
+        <PaginationComponent />
+      </div>
     </div>
   );
-};
+}
 
 export default App;
